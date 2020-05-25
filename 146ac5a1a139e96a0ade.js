233a0aebf86dda1f6f01f00ca1e295fbc09d2881
v.aristarkhova <Veronika.Aristarkhova@acronis.com>
2020-05-25 16:08:11 +0300

    Fix after review

---
+++ 146ac5a1a139e96a0ade.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{79:function(e,s,t){"use strict";t.r(s);var i=function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"works"},[this._v("Works")])};i._withStripped=!0;var n={name:"Works"},r=t(4),a=Object(r.a)(n,i,[],!1,null,"2ffee08e",null);a.options.__file="src/admin/pages/Works.vue";s.default=a.exports}}]);
