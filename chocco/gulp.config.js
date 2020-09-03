a52e119932ecd92d9e21080a4913137f332a1b42
v.aristarkhova <Veronika.Aristarkhova@acronis.com>
2020-09-03 17:57:31 +0300

    Use gulp for making builds

---
+++ chocco/gulp.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    SRC_PATH : 'src',
+    DIST_PATH : 'dist',
+    STYLE_LIBS : [
+        'node_modules/normalize.css/normalize.css'
+    ],
+    JS_LIBS : [
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/fancybox/dist/js/jquery.fancybox.js',
+        'node_modules/bxslider/dist/jquery.bxslider.js',
+        'node_modules/jquery-touchswipe/jquery.touchSwipe.js',
+        'node_modules/mobile-detect/mobile-detect.js'
+    ]
+};
